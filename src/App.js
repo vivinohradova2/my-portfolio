4baeb58879fc56b17a3970381c1ed0d45683a644
Viktoriia <133902609+vivinohradova@users.noreply.github.com>
2024-08-21 10:59:25 +0300

    arrow folder

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import Works from "./components/Works/Works";
 import Tooling from "./components/Tooling/Tooling";
+import { FaArrowUp } from "react-icons/fa";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Works />
       <Contacts />
       <Footer />
+      <FaArrowUp />
     </>
   );
 }
