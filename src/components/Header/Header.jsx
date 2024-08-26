78e186c60fceeee2c88b3fdd3be8909d998167f6
Viktoriia <133902609+vivinohradova@users.noreply.github.com>
2024-08-26 17:46:04 +0300

    hamburger on mobile v

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import logo from "../../assets/images/logo.png";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0); 
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleClickHamburger = () => {
     setIsOpen(!isOpen);
@@ -14,8 +14,8 @@ const Header = () => {
 
   const handleNavClick = (index, sectionId) => {
     setCurrentIndex(index);
-    document.getElementById(sectionId).scrollIntoView({behavior: 'smooth'});
-    setIsOpen(false)
+    document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+    setIsOpen(false);
   };
 
   return (
@@ -24,70 +24,77 @@ const Header = () => {
         <section id="home" className={styles.container}>
           <div className={styles.top}>
             <img src={logo} alt="icon" />
+
             <nav className={styles.hamburger}>
               <FaHamburger onClick={handleClickHamburger} />
             </nav>
           </div>
+          <div className={styles.mobile}>
+            <div className={` ${isOpen ? styles.menuOpen : styles.menuClose}`}>
+              <div className={styles.social}>
+                <a
+                  href="https://www.linkedin.com/in/viktoriia-vynohradova/"
+                  target="blank"
+                >
+                  <p>Linkedin</p>
+                </a>
 
-          <div className={` ${isOpen ? styles.menuOpen : styles.menuClose}`}>
-            <div className={styles.social}>
-              <a
-                href="https://www.linkedin.com/in/viktoriia-vynohradova/"
-                target="blank"
-              >
-                <p>Linkedin</p>
-              </a>
+                <a
+                  href="https://www.instagram.com/frontend_vika/"
+                  target="blank"
+                >
+                  <p>Instagram</p>
+                </a>
+                <a href="https://t.me/vinohradova_frontend" target="blank">
+                  <p>Telegram</p>
+                </a>
+              </div>
 
-              <a href="https://www.instagram.com/frontend_vika/" target="blank">
-                <p>Instagram</p>
-              </a>
-              <a href="https://t.me/vinohradova_frontend" target="blank"><p>Telegram</p></a>
+              <nav className={styles.menu}>
+                <li
+                  className={`${currentIndex === 0 ? styles.active : ""}`}
+                  onClick={() => handleNavClick(0, "home")}
+                >
+                  <FaArrowRight className={styles.arrow} />
+                  Home
+                </li>
+                <li
+                  className={`${currentIndex === 1 ? styles.active : ""}`}
+                  onClick={() => handleNavClick(1, "about")}
+                >
+                  <FaArrowRight className={styles.arrow} />
+                  About me
+                </li>
+                <li
+                  className={`${currentIndex === 2 ? styles.active : ""}`}
+                  onClick={() => handleNavClick(2, "tooling")}
+                >
+                  <FaArrowRight className={styles.arrow} />
+                  Tooling
+                </li>
+                <li
+                  className={`${currentIndex === 3 ? styles.active : ""}`}
+                  onClick={() => handleNavClick(3, "works")}
+                >
+                  <FaArrowRight className={styles.arrow} />
+                  Works
+                </li>
+                <li
+                  className={`${currentIndex === 4 ? styles.active : ""}`}
+                  onClick={() => handleNavClick(4, "contacts")}
+                >
+                  <FaArrowRight className={styles.arrow} />
+                  Contacts
+                </li>
+                <li
+                  className={`${currentIndex === 5 ? styles.active : ""}`}
+                  onClick={() => handleNavClick(5, "resume")}
+                >
+                  <FaArrowRight className={styles.arrow} />
+                  Resume
+                </li>
+              </nav>
             </div>
-
-            <nav className={styles.menu}>
-              <li
-                className={`${currentIndex === 0 ? styles.active : ""}`}
-                onClick={() => handleNavClick(0, "home")}
-              >
-                <FaArrowRight className={styles.arrow} />
-                Home
-              </li>
-              <li
-                className={`${currentIndex === 1 ? styles.active : ""}`}
-                onClick={() => handleNavClick(1, "about")}
-              >
-                <FaArrowRight className={styles.arrow} />
-                About me
-              </li>
-              <li
-                className={`${currentIndex === 2 ? styles.active : ""}`}
-                onClick={() => handleNavClick(2, "tooling")}
-              >
-                <FaArrowRight className={styles.arrow} />
-                Tooling
-              </li>
-              <li
-                className={`${currentIndex === 3 ? styles.active : ""}`}
-                onClick={() => handleNavClick(3, "works")}
-              >
-                <FaArrowRight className={styles.arrow} />
-                Works
-              </li>
-              <li
-                className={`${currentIndex === 4 ? styles.active : ""}`}
-                onClick={() => handleNavClick(4, "contacts")}
-              >
-                <FaArrowRight className={styles.arrow} />
-                Contacts
-              </li>
-              <li
-                className={`${currentIndex === 5 ? styles.active : ""}`}
-                onClick={() => handleNavClick(5, "resume")}
-              >
-                <FaArrowRight className={styles.arrow} />
-                Resume
-              </li>
-            </nav>
           </div>
         </section>
       </header>
